6806136d9e1a3174d33f0c125d8a66b51f3e643d
Muslik <ahmetaliev345@gmail.com>
2025-04-04 23:57:31 +0500

    [add] : vacancy service

--- lab10/frontend/hh-front/src/app/services/vacancies.service.ts
+++ lab10/frontend/hh-front/src/app/services/vacancies.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Company } from '../models/company';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VacanciesService {
+  readonly apiUrl = "http://127.0.0.1:8000/api/vacancies"
+  private http = inject(HttpClient);
 
-  constructor() { }
+  getAllVacancies(): Observable<Company[]> {
+    return this.http.get<Company[]>(this.apiUrl)
+  }
 }
